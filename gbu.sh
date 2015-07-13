53c5a7b52ae7bf43bf0bc69b0a9f310f915afec6
ZeonWang <scplay@gmail.com>
2015-07-13 02:02:21 -0700

    2015-07-13 02:02:21 Update

--- gbu.sh
+++ gbu.sh
@@ -2,6 +2,6 @@
 #echo "Back up to Github!"
 cd /root/git/gitTest
 upTime=`date "+%Y-%m-%d %H:%M:%S"`
-cmt=${upTime}' Update'
-/usr/bin/git commit -am "$upTime"
+cmt=${upTime}" Update"
+/usr/bin/git commit -am "$cmt"
 /usr/bin/git push origin master 
