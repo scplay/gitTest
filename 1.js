89206cefeb094460fa752de536ad8e5e6332cd41
ZeonWang <scplay@gmail.com>
2015-07-13 01:20:01 -0700

    Update Today

--- 1.js
+++ 1.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 http.createServer(function (request, response) {
   response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end('SuperVisor is Ok? Auto Back Up\n');
+  response.end('SuperVisor is Ok? ?CronTab So Fuck Auto Back Up\n');
 }).listen(80);
 
 console.log('Server running at http://127.0.0.1:80/');
