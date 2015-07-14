2befa4bd8387028ac0c659d8f198f4f01b0af222
ZeonWang <scplay@gmail.com>
2015-07-14 14:42:52 +0800

    2015-07-14 14:42:52Zeon Auto Push

--- gbu.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#echo "Back up to Github!"
-cd /root/git/gitTest
-upTime=`date "+%Y-%m-%d %H:%M:%S"`
-cmt=${upTime}" Update"
-/usr/bin/git commit -am "$cmt"
-/usr/bin/git push origin master 
